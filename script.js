31f33d1d77069bbd8c97e5ab8855febb0c0d5457
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-02-08 02:11:22 +0300

    new: search bar style

--- script.js
+++ script.js
@@ -255,4 +255,12 @@ checkbox.addEventListener("change", () => {
 
 })
 
+const searchBox = document.getElementById('searchBox'),
+    searchIcon = document.getElementById('searchIcon');
+
+searchIcon.onclick = function () {
+    searchBox.classList.toggle('active');
+};
+
+
 document.addEventListener("DOMContentLoaded", autorun);
